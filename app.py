5b04284f3ea71fdcaf0f31e3e3ea981262e54881
aneeqahamid <146227282+aneeqahamid@users.noreply.github.com>
2023-09-28 14:59:30 +0000

    here is my AI based web appliction

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-  return "hello_world"
+def Hello_world():
+  return render_template('home.html')
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
